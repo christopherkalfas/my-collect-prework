13cb648073909c2e393cac4a76c96f426d237103
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-12 16:46:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 def my_collect(array)
   i = 0 
-  
+  returned_info =[]
   if block_given?
     while i < array.length 
-    yield array[i]
+    returned_info << yield array[i]
       i =i + 1 
     end 
   end
+  returned_info
 end 
 
